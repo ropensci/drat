294884f4f51c8d5bdc50e880047a21284adf9746
Carl Boettiger <cboettig@gmail.com>
2015-08-12 17:39:44 -0700

    more informative error (warning)

--- deploy_s3.R
+++ deploy_s3.R
@@ -3,15 +3,15 @@ library("aws.s3")
 s3copy <- function(path, bucket, recursive = FALSE, ...){
   who <- list.files(path = path, recursive = recursive)
   sapply(who, function(f){
-    putobject(
+    p <- putobject(
       file = paste(path, f, sep="/"),
       bucket = bucket, 
       object = paste(path, f, sep="/"),
       ...)
+    if(!is.logical(p)){
+      warning(paste("uploading", f, "failed:", p$Message))
+    }
   })
 }
 status <- s3copy("src", "packages.ropensci.org", recursive = TRUE, region="us-west-2", 
        key = Sys.getenv("AWS_ACCESS_KEY_ID"), secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
-
-if(!all(status))
-  stop("Some packages failed to upload")
