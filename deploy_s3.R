49785c2101650878b2369cbbda10862898604594
Carl Boettiger <cboettig@gmail.com>
2015-08-12 17:05:14 -0700

    fix deploy path issues (hopefully)

--- deploy_s3.R
+++ deploy_s3.R
@@ -2,13 +2,14 @@
 library("aws.s3")
 s3copy <- function(path, bucket, recursive = FALSE, ...){
   who <- list.files(path = path, recursive = recursive)
-  for(f in who){
-    p <- putobject(
-      file = f,
+  lapply(who, function(f){
+    putobject(
+      file = paste(path, f, sep="/"),
       bucket = bucket, 
-      object = basename(f),
+      object = paste(path, f, sep="/"),
       ...)
-  }
+  })
 }
-s3copy("src", "packages.ropensci.org", recursive = TRUE, region="us-west-2", 
+status <- s3copy("src", "packages.ropensci.org", recursive = TRUE, region="us-west-2", 
        key = Sys.getenv("AWS_ACCESS_KEY_ID"), secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
+
