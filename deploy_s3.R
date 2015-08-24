898a1afc6cd60fcc68e2b818514136a6c671486e
Carl Boettiger <cboettig@gmail.com>
2015-08-24 15:48:56 -0700

    pass env vars to docker

--- deploy_s3.R
+++ deploy_s3.R
@@ -1,4 +1,6 @@
 
+print(Sys.getenv("AWS_DEFAULT_REGION"))
+
 library("aws.s3")
 s3copy <- function(path, bucket, recursive = FALSE, ...){
   who <- list.files(path = path, recursive = recursive)
@@ -13,7 +15,10 @@ s3copy <- function(path, bucket, recursive = FALSE, ...){
     }
   })
 }
-status <- s3copy("src", "packages.ropensci.org", recursive = TRUE, region="us-west-2", 
-       key = Sys.getenv("AWS_ACCESS_KEY_ID"), secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
+status <- s3copy("src", "packages.ropensci.org", 
+                 recursive = TRUE, 
+                 region="us-west-2", 
+                 key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
+                 secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
 
 warnings()
