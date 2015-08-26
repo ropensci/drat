532308f4872c324243dddb04c9bc015aa2e1a2d3
Carl Boettiger <cboettig@gmail.com>
2015-08-26 11:24:18 -0700

    update push address

--- git_deploy.sh
+++ git_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git config --global user.email 'carl@ropensci.org' && git config --global user.name 'Carl Boettiger'
-git add *.json && git commit -m 'built and deployed from circle [ci skip]' && git push https://cboettig:${GH_TOKEN}@github.com/cboettig/drat gh-pages 
+git add *.json && git commit -m 'built and deployed from circle [ci skip]' && git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
 
