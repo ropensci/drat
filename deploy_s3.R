b0a7ec8cd1f619229ab3892bc029d7b130566141
Carl Boettiger <cboettig@gmail.com>
2015-08-26 09:41:52 -0700

    update deploy to include computation of logs

--- deploy_s3.R
+++ deploy_s3.R
@@ -1,7 +1,6 @@
-
-print(Sys.getenv("AWS_DEFAULT_REGION"))
-
 library("aws.s3")
+
+
 s3copy <- function(path, bucket, recursive = FALSE, ...){
   who <- list.files(path = path, recursive = recursive)
   sapply(who, function(f){
@@ -10,15 +9,22 @@ s3copy <- function(path, bucket, recursive = FALSE, ...){
       bucket = bucket, 
       object = paste(path, f, sep="/"),
       ...)
+    closeAllConnections()
     if(!is.logical(p)){
       warning(paste("uploading", f, "failed:", p$Message))
     }
   })
 }
+
+## Upload packages to S3
 status <- s3copy("src", "packages.ropensci.org", 
                  recursive = TRUE, 
                  region="us-west-2", 
                  key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
                  secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
-
 warnings()
+
+## Parse the download logs, update the log summary, delete the raw log files
+source("parse_s3_logs.R")
+
+
