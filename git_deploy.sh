6e22b0c6776bbe4a8e08a9b6928b01443626328b
Carl Boettiger <carl@ropensci.org>
2015-09-03 09:33:22 -0700

    fix git deploy syntax

--- git_deploy.sh
+++ git_deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
 set -e
 
-{ git pull } &> /dev/null 2>&1
+{
+  git pull 
+} > /dev/null 2>&1
+
 git config --global user.email 'carl@ropensci.org' && git config --global user.name 'Carl Boettiger'
 echo $(date) > date.txt
 git add *.json date.txt
 git commit -m 'built and deployed from circle [ci skip]' 
-{
-git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
+{ 
+  git push https://cboettig:$GH_TOKEN@github.com/ropensci/drat gh-pages 
 } &> /dev/null 2>&1
+
 
