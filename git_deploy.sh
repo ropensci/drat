d29aa726be98a52ce7bea9dff78c2988a51454f8
Carl Boettiger <cboettig@gmail.com>
2015-09-03 09:08:50 -0700

    testing git deploy script

--- git_deploy.sh
+++ git_deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+{ git pull } &> /dev/null 2>&1
 git config --global user.email 'carl@ropensci.org' && git config --global user.name 'Carl Boettiger'
-echo $(date) > date.txt && git add *.json date.txt && git commit -m 'built and deployed from circle [ci skip]' && git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
+echo $(date) > date.txt
+git add *.json date.txt
+git commit -m 'built and deployed from circle [ci skip]' 
+{
+git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
+} &> /dev/null 2>&1
 
