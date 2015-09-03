ff89f29a63ef978c90e7bb5e4063c996b2bd7bbc
Carl Boettiger <cboettig@gmail.com>
2015-09-02 21:05:43 -0700

    add PACKAGES file

--- git_deploy.sh
+++ git_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git config --global user.email 'carl@ropensci.org' && git config --global user.name 'Carl Boettiger'
-git add *.json && git commit -m 'built and deployed from circle [ci skip]' && git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
+echo $(date) > date.txt && git add *.json date.txt && git commit -m 'built and deployed from circle [ci skip]' && git push https://cboettig:${GH_TOKEN}@github.com/ropensci/drat gh-pages 
 
