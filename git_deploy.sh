21ba740e353982744e0e1692b5b86f52fb500b88
Carl Boettiger <carl@ropensci.org>
2015-09-08 14:55:57 -0700

    Just drop .json files for the moment

--- git_deploy.sh
+++ git_deploy.sh
@@ -7,7 +7,8 @@ set -e
 
 git config --global user.email 'carl@ropensci.org' && git config --global user.name 'Carl Boettiger'
 echo $(date) > date.txt
-git add *.json date.txt
+#git add *.json 
+git add date.txt
 git commit -m 'built and deployed from circle [ci skip]' 
 { 
   git push https://cboettig:$GH_TOKEN@github.com/ropensci/drat gh-pages 
