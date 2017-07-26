b7a1c6562cff151d2421209b926f8e5fa5962943
Carl Boettiger <cboettig@gmail.com>
2017-07-25 18:07:35 -0700

    need to update PACKAGES index too!

--- git_deploy.sh
+++ git_deploy.sh
@@ -9,7 +9,7 @@ git config --global user.email 'carl@ropensci.org' && git config --global user.n
 echo $(date) > date.txt
 git add *.json 
 git add date.txt
-git add -f src/contrib/*.tar.gz
+git add -f src/contrib/*
 git commit -m 'built and deployed from circle [ci skip]' 
 { 
   git push https://cboettig:$GH_TOKEN@github.com/ropensci/drat gh-pages 
